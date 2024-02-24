aee060a58c031f8ee4ecc77011929c1992cdc5c8
anurag <anuanurag248@gmail.com>
2024-02-24 10:39:54 +0530

    file added

---
+++ LaptopApp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+app_name='lap'
+urlpatterns = [
+    path('',views.Home,name='home'),
+    path('laps/',views.laps,name='laps'),
+    path('<slug:c_slug>/',views.laps,name='product_by_category'),
+    path('details/<int:id>',views.Details,name='details'),
+]
